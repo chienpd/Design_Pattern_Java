f4b73b7bc696364649cf854a0da98be355c80dba
chienpham <phamdinhchienk58@gmail.com>
2017-05-18 16:11:49 +0700

    Create concrete classes implementing the same interface.

---
+++ src/Abstract_Factory_Pattern/Circle.java
@@ -0,0 +1,11 @@
+package Abstract_Factory_Pattern;
+
+/**
+ * Created by pdc on 18/05/2017.
+ */
+public class Circle implements Shape {
+    @Override
+    public void draw() {
+        System.out.println("Inside Circle::draw() method.");
+    }
+}
---
+++ src/Abstract_Factory_Pattern/Square.java
@@ -0,0 +1,11 @@
+package Abstract_Factory_Pattern;
+
+/**
+ * Created by pdc on 18/05/2017.
+ */
+public class Square implements Shape {
+    @Override
+    public void draw() {
+        System.out.println("Inside Square::draw() method.");
+    }
+}
