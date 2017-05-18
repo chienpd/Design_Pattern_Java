555ee91a97c16a29ff38fd9a7233e3987235c7e4
chienpham <phamdinhchienk58@gmail.com>
2017-05-18 15:43:41 +0700

    Create concrete classes implementing the same interface.

---
+++ src/Factory_Pattern/Rectangle.java
@@ -0,0 +1,11 @@
+package Factory_Pattern;
+
+/**
+ * Created by pdc on 18/05/2017.
+ */
+public class Rectangle implements Shape {
+    @Override
+    public void draw() {
+        System.out.println("Inside Rectangle::draw() method.");
+    }
+}
